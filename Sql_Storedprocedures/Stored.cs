74cd6d2fef25b6bf08974642e5403ebbb0879773
sreemohan1198 <128161554+sreemohan1198@users.noreply.github.com>
2023-04-02 18:29:06 -0500

    Add files via upload
    STORED PROCEDURE OPERATIONS
    -CRUD OPERATIONS PERFORMED 
    -TEACHER DB

---
+++ Sql_Storedprocedures/Stored.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+using static ClassLibrary2.Student;
+using static ClassLibrary2.Course;
+
+namespace Sql_Storedprocedures
+{
+    public class Stored
+    {
+        public void Retrivesp()
+        {
+            String connString = "Data Source=.;Initial Catalog=TEACHER_STUDENT;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            conn.Open();
+            //cmd.CommandText = "INSERT INTO [dbo].[TEACHER](TEACHER_ID,TEACHER_NAME,AGE)\r\n VALUES (@TEACHER_ID,@TEACHER_NAME,@AGE)";
+            //cmd.Parameters.AddWithValue("@TEACHER_ID", teach.TEACHER_ID);
+            //cmd.Parameters.AddWithValue("@TEACHER_NAME", teach.TEACHER_NAME);
+            //cmd.Parameters.AddWithValue("@AGE", teach.AGE);
+
+            cmd.CommandText = "spGetStudentnameCoursename";
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = cmd.ExecuteReader();
+            //cmd.Parameters.AddWithValue("@STUDENT_NAME", student.STUDENT_NAME);
+            //cmd.Parameters.AddWithValue("@COURSE_NAME", course.COURSE_NAME);
+            while (reader.Read())
+            {
+                Student student = new Student();
+                Course course = new Course();
+                student.STUDENT_NAME = reader[0].ToString();
+                course.COURSE_NAME = reader[1].ToString();
+                Console.WriteLine(reader[0].ToString() + " - " + reader[1].ToString());
+
+            }
+        }
+
+
+        //------------STORED PROCEDURE INSERTION----------------------//
+        public void Insertsp(Teacher teach)
+        {
+            String connString = "Data Source=.;Initial Catalog=TEACHER_STUDENT;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+           // conn.Open();
+            cmd.CommandText = "sdteach";
+            cmd.CommandType = CommandType.StoredProcedure;
+            //SqlDataReader reader = cmd.ExecuteReader();
+           /* cmd.CommandText = @"INSERT INTO[dbo].[sdteach] ([TEACHER_ID],[TEACHER_NAME],[AGE])
+                                VALUES(@TEACHER_ID,@TEACHER_NAME,@AGE)"; */
+            cmd.Parameters.AddWithValue("@TEACHER_ID", teach.TEACHER_ID );
+            cmd.Parameters.AddWithValue("@TEACHER_NAME",teach.TEACHER_NAME);
+            cmd.Parameters.AddWithValue("@AGE",teach.AGE);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+            }
+            finally
+            {
+                conn.Close();
+                Console.Read();
+            }
+            Console.Read();
+            //while (reader.Read())
+            //{
+            //    Teacher teacher = new Teacher();
+            //    teacher.TEACHER_ID = reader[0].GetHashCode();
+            //    teacher.TEACHER_NAME = reader[1].ToString();
+            //    teacher.AGE= reader[2].GetHashCode();
+
+            //}
+            // conn.Close();
+            // Console.ReadLine();
+        }
+
+        //------------------STORED PROCEDURE UPDATE------------------------//
+
+        //public void Updatesp()
+        //{
+        //    String connString = "Data Source=.;Initial Catalog=TEACHER_STUDENT;Integrated Security=True";
+        //    SqlConnection conn = new SqlConnection(connString);
+        //    SqlCommand cmd = new SqlCommand();
+        //    cmd.Connection = conn;
+        //    conn.Open();
+        //    cmd.CommandText = " ";
+        //    cmd.CommandType = CommandType.StoredProcedure;
+        //    SqlDataReader reader = cmd.ExecuteReader();
+        //    cmd.CommandText = " ";
+        //    //cmd.Parameters.AddWithValue("@T", 8);
+          
+        //    while (reader.Read())
+        //    {
+               
+
+        //    }
+        //    conn.Close();
+        //    Console.ReadLine();
+        //}
+
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+//Course c = new Course();
+//c.COURSE_NAME = reader[1].ToString();
+//    try
+//    {
+//        conn.Open();
+//        cmd.ExecuteNonQuery();
+//    }
+//    catch (Exception e)
+//    {
+
+//        Console.WriteLine("Error Generated. Details: " + e.ToString());
+//    }
+//    finally
+//    {
+//        conn.Close();
+//        Console.Read();
+//    }
+//Console.ReadLine();
---
+++ Sql_Storedprocedures/TeacherMain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+using static ClassLibrary2.Course;
+using static ClassLibrary2.Student;
+
+namespace Sql_Storedprocedures
+{
+    public class TeacherMain
+    {
+        static void Main(string[] args)
+        {
+
+           Stored stored = new Stored();
+            Teacher teacher = new Teacher();
+            teacher.TEACHER_ID = 14;
+            teacher.TEACHER_NAME = "jack";
+                teacher.AGE = 36;
+            stored.Insertsp(teacher);
+            //stored.Retrivesp();
+            //stored.Insertsp();
+
+           
+
+            
+
+
+
+
+
+
+
+            //List<Teacher> t = new List<Teacher>();
+            //Crud_oper crud = new Crud_oper();
+            //Teacher teacher = new Teacher();
+            //t.TEACHER_ID = 7;
+            //t.TEACHER_NAME = 'Mahesh';
+            //t.AGE = 26;
+            //Crud.Insert(t);
+
+            //try
+            //{
+            //    conn.Open();
+            //    cmd.ExecuteNonQuery();
+            //}
+            //catch (Exception e)
+            //{
+
+            //    Console.WriteLine("Error Generated. Details: " + e.ToString());
+            //}
+            //finally
+            //{
+            //    conn.Close();
+            //    Console.Read();
+            //}
+
+
+
+
+        }
+
+    }
+}
