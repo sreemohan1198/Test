1cea565217bd98e54f2179035d620746e0ad3a7d
sreemohan1198 <128161554+sreemohan1198@users.noreply.github.com>
2023-04-02 18:26:09 -0500

    Add files via upload
    OBJECT CLASS EXAMPLES

---
+++ Obj_Cls_Examples/Execution.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obj_Cls_Examples
+{
+    //    internal class Execution
+    //    {
+    //        String name, city;
+    //        int age;
+    //        public void acceptdetails()
+    //        {
+    //            Console.WriteLine("Enter Name :" +name);
+    //            Console.ReadLine();
+
+    //            Console.WriteLine("Enter city : "+city);
+    //            Console.ReadLine();
+
+    //            Console.WriteLine("Enter age :"+age);
+    //            Console.ReadLine();
+    //        }
+
+    //    }
+
+    //class Animal //base class or parent class
+    //{
+    //    public string name;
+    //    public void dogName()  //method is created for parent class
+    //    {
+    //        Console.WriteLine("Golden Retriever!");
+    //    }
+    //}
+
+    //class Dog : Animal  //derived class or child class
+    //{
+    //    public void getName()//new method is created to Dog class 
+    //    {
+    //        Console.WriteLine("This is a Dog :" + name);//inherits name property from parent class
+    //    }
+    //}
+
+    //public class Student
+    //{
+    //    public string firstname;
+    //    public string secondname;
+    //    public int age;
+    //    public string course;
+
+    //    public void details()
+    //    {
+    //        Console.WriteLine("First name :" + firstname);
+    //        Console.WriteLine("Second name :" + secondname);
+    //        Console.WriteLine("Age:" + age);
+    //        Console.WriteLine("Course:" + course);
+    //    }
+    //}
+    //public class Courses : Student
+    //{
+    //    public void coursedetails()
+    //    {
+    //        Console.WriteLine("Student courses:" + course);
+    //    }
+    //}
+
+
+    //public class Person
+    //{
+    //    public string Fname { get; set; }
+    //    public string Lname { get; set; }
+    //    public int Id { get; set; }
+    //}
+
+    //public class Employee:Person
+    //    {
+    //        public string EName { get; set; }
+    //        public string Dept { get; set; }
+    //        public int Salary { get; set; }
+    //    }
+
+
+    public class Security
+    {
+        public String Boss="Good Morning Sir...";
+        public String Employee="Welcome Sir";
+        public String Customer="Id please";
+
+        public virtual void Sguard()
+        {
+            Console.WriteLine("Hello!!" + Boss);
+            Console.WriteLine("Good Morning" + Employee);
+            Console.WriteLine("Show Me your" + Customer);
+        }
+    }
+    public class Boss : Security
+    {
+        public override void Sguard()
+        {
+            Console.WriteLine("Have a Nice day");
+            //Console.WriteLine("Welcome sir!" + Employee);
+            //Console.WriteLine("Show Me you ID" + Customer);
+        }
+    }
+
+    public class Employee : Security
+    {
+        public override void Sguard()
+        {
+            //Console.WriteLine("Good Morning ,Sir!!" + Boss);
+            Console.WriteLine("Have a Good day");
+            //Console.WriteLine("Show Me you ID" + Customer);
+        }
+    }
+
+    public class Customer : Security
+    {
+        public override void Sguard()
+        {
+            //Console.WriteLine("Good Morning ,Sir!!" + Boss);
+            //Console.WriteLine("Welcome sir!" + Employee);
+            Console.WriteLine("You may go..");
+        }
+    }
+
+}
+
+
+
+
+
+
---
+++ Obj_Cls_Examples/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using static Obj_Cls_Examples.Student;
+
+namespace Obj_Cls_Examples
+{
+    internal class Program
+    {
+        //    static void Main(string[] args)
+        //    {
+        //        Execution p=new Execution();
+        //        p.acceptdetails();            
+        //        Console.ReadLine();
+        //    }
+        //}
+
+        //    {
+        //        //example prog for inheritance
+        //        static void Main(string[] args)
+        //        {
+        //            Dog pug = new Dog();//create a obj named pug to Dog class 
+        //            pug.name = "Marley";//assign vale to name property which is in parent class
+        //            pug.dogName();//calling a created obj with method defined in parent class
+
+
+        //            pug.getName();//accessing method from own class
+        //            Console.ReadLine();
+        //        }
+
+        //    }
+        //}
+        //{
+        //static void Main(string[] args)
+        //{
+        //    Student std = new Student();
+        //    std.firstname = "Sri";
+        //    std.secondname = "devi";
+        //    std.age = 20;
+        //    std.course = "CSE";
+        //    std.details();
+
+        //    Courses cd = new Courses();
+        //    cd.coursedetails();
+        //    Console.ReadLine();
+
+        //}
+
+        //}
+
+
+
+        //static void Main(string[] args)
+        //{
+
+        //    Person p1 = new Person();
+        //    p1.Fname = "sri";
+        //    p1.Lname = "lakshmi";
+        //    p1.Id = 1;
+
+
+        //    Employee emp = new Employee();
+        //    emp.Fname = "Sree";
+        //    emp.Lname = "Mohan";
+        //    emp.Id = 10;
+        //    emp.Dept = "Developer";
+        //    emp.Salary = 50000;
+
+        //    Console.Read();
+
+        //}
+
+
+
+
+        public static void Main(string[] args)
+        {
+
+            Security[] sgreet = new Security[4];
+            sgreet[0] = new Security();
+            sgreet[1] = new Boss();
+            sgreet[2] = new Employee();
+            sgreet[3] = new Customer();
+
+            //Security sgreet = new Security();
+            //sgreet.Boss=Sguard();
+            ////Boss sb = new Boss();
+            //sgreet.Sguard();
+            //sb.Sguard();
+            //sgreet.Employee = new Employee();
+            // sgreet.Customer = new Customer();
+
+
+            foreach (Security s in sgreet)
+            {
+                sgreet.Sguard();
+            }
+            Console.Read();
+        }
+
+    }
+}
+
+
